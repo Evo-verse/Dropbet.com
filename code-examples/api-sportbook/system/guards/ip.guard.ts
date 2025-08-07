2124f992a1f0005ea9cf71e8e46ca81380d32a0d
DeadAstri <liksonic@gmail.com>
2025-08-07 09:51:56 +0300

    fixes_hide_ips_list

--- code-examples/api-sportbook/system/guards/ip.guard.ts
+++ code-examples/api-sportbook/system/guards/ip.guard.ts
@@ -5,30 +5,13 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class SportsbookIpGuard implements CanActivate {
   private static readonly GR8_VPN_IPS = new Set<string>([
-    '66.159.223.53',
-    '66.159.223.59',
-    '208.127.12.158',
-    '208.127.12.159',
-    '208.127.105.155',
-    '208.127.105.37',
-    '130.41.59.141',
-    '130.41.218.83',
-    '130.41.34.134',
-    '208.127.41.82',
-    '130.41.235.243',
-    '130.41.235.244',
-    '130.41.188.74',
-    '130.41.188.72',
-    '128.77.3.181',
-    '128.77.3.66',
-    '165.85.185.236',
-    '165.85.185.237',
+    'private',
+    'private_1',
   ]);
 
   private static readonly GR8_PRODUCTION_IPS = new Set<string>([
-    '3.121.28.124',
-    '3.121.38.70',
-    '3.121.23.59',
+    'private',
+    'private_1',
   ]);
 
   private static readonly ALLOWED_IPS = new Set<string>([
